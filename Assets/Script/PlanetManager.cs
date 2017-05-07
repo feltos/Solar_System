063c3fb2e6aa5d3ede5b7117b7f6d590fb4e89b3
guillaume volgyesi <guillaume volgyesi>
2017-05-07 20:04:55 +0200

    Don't work correctly, try to fix

--- Assets/Script/PlanetManager.cs
+++ Assets/Script/PlanetManager.cs
@@ -6,11 +6,10 @@ using UnityEngine;
 public class PlanetManager : MonoBehaviour 
 {
     [SerializeField]GameObject Planet;
-    Vector3 Gravity;
     [SerializeField]
     Vector3 Direction;
     Vector3 Distance;
-    Vector3 ConstanteG = new Vector3(6,0,0);
+    Vector3 Velocity;
     [SerializeField]
     float Mass;
     [SerializeField]float PlanetMass;
@@ -22,18 +21,17 @@ public class PlanetManager : MonoBehaviour
 
     void Start ()
 	{
-		    
+       
     }
 	
 	void Update () 
 	{
-       
+        
     }
     void FixedUpdate()
     {
-        Distance = (Planet.transform.position - transform.position);
-        Gravity = (Distance * (PlanetMass * Mass) / Mathf.Pow(
-            (transform.position - Planet.transform.position).magnitude, 2.0f));
-        transform.position += Direction + Gravity * Time.deltaTime;
+        Distance = (Planet.transform.position - transform.position).normalized;
+        Velocity = Distance * (PlanetMass * Mass) / Mathf.Pow((transform.position - Planet.transform.position).magnitude,2.0f);
+        transform.position += Velocity * Time.deltaTime + Direction;  
     }
 }
